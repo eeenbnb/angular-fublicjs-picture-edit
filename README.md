210805ddc931f942b5a67aec4e545d40644dbd09
eeenbnb <eee.kz.www@gmail.com>
2019-01-31 21:33:41 +0900

    picture edit

--- README.md
+++ README.md
@@ -1,27 +1,41 @@
-# AngularFublicjsPictureEdit
+#### develop memo
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
+##### first step. npm package install.
+```
+npm install --no-optional --save fabric
+```
+`--no-optional` : Because don't use node-canvas.
 
-## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+##### AppComponent imports
+`import { fabric } from 'fabric';`
 
-## Code scaffolding
+canvas element set AppComponent HTML
+```
+<canvas id="canvas" width="600" height="600" #canvas></canvas>
+```
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+```
+canvas:any;
 
-## Build
+ngOnInit(){
+  this.canvas = new fabric.Canvas('canvas', {
+     isDrawingMode: true,
+     selection: true,
+     stateful: true
+   });
+}
+```
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+##### AddingPicture
+```
+<input type="file" (change)="onChangeInput($event)"/>
+```
+onChangeInput method get the picture.  
+Rendering to canvas.
 
-## Running unit tests
+##### Adding picture on sticker
+addImage to Canvas.
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+##### out picture
+`canvasElement.toDataURL("image/png")`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  @ViewChild("preview")previewElement:ElementRef;
+  @ViewChild("canvas")canvasElement:ElementRef;
+  canvas:any;
+  assetsList:any = [
+    "/assets/img/twitter_logo.png",
+    "/assets/img/unko.png",
+    "/assets/img/emoji100.png",
+    "/assets/img/baby.png",
+    "/assets/img/kinniku.png"
+  ];
+
+  ngOnInit(){
+    this.canvas = new fabric.Canvas('canvas', {
+       isDrawingMode: false,
+       selection: true,
+       stateful: true
+     });
+  }
+
+  onChangeInput(e){
+    let file = e.target.files[0];
+    let reader = new FileReader();
+
+    if(file.type.indexOf("image") < 0){
+      console.log("error")
+      return false;
+    }
+
+    reader.onload = ((file)=> {
+      return (e)=> {
+        let image = new Image;
+        image.onload = ()=>{
+          let fabricImage = new fabric.Image(image);
+
+          fabricImage.set({
+            selectable: false,
+            width:  fabricImage.width,
+            height: fabricImage.height
+          });
+
+          this.canvas.setWidth(fabricImage.width);
+          this.canvas.setHeight(fabricImage.height);
+          this.canvas.clear();
+
+          this.canvas.add(fabricImage);
+        };
+        image.src = e.target.result;
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  }
+
+  addSticker(i){
+    let image = new Image;
+    image.onload = ()=>{
+      let fabricImage = new fabric.Image(image);
+
+      fabricImage.set({
+        selectable: true,
+        width:  fabricImage.width,
+        height: fabricImage.height
+      });
+
+      this.canvas.add(fabricImage);
+    };
+    image.src = this.assetsList[i];
+  }
+
+  canvasToImg(){
+    this.previewElement.nativeElement.src = this.canvasElement.nativeElement.toDataURL("image/png");
+  }
+
 }
